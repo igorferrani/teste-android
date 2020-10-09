f59845077a924c0676c1f5a74254c7aa66246669
Igor Ferrani <igor.ferrani@gmail.com>
2020-10-09 14:11:42 -0300

    Refatorando localização de arquivos que estavam em pacotes errados

--- EasyInvest/app/src/main/java/br/com/easynvest/simulation/state/SimulationFormInteractor.kt
+++
@@ -1,6 +0,0 @@
-package br.com.easyinvest.simulation.state
-
-sealed class SimulationFormInteractor {
-    data class ToSimulation(val investedAmount: Double, val maturityDate: String, val rate: Double) :
-        SimulationFormInteractor()
-}
